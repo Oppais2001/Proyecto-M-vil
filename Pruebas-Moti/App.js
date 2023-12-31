e2904402bd047fe9c85676bc3d0a78280f35cdea
Felipe <118953310+Oppais2001@users.noreply.github.com>
2023-12-31 13:39:33 -0300

    pruebas moti

--- Pruebas-Moti/App.js
+++ Pruebas-Moti/App.js
@@ -1,18 +1,21 @@
 import { useReducer } from 'react';
 import { StyleSheet, Pressable } from 'react-native';
-import { MotiImage } from 'moti';
+import { MotiImage, MotiView } from 'moti';
 
 function Shape() {
   return (
     <MotiImage
       from={{
-        opacity: 0,
+        height: 400,
+        scaleX: 1,
       }}
       animate={{
-        opacity: 1,
+        height: 0,
+        scaleX: 1,
       }}
       transition={{
         type: 'timing',
+        duration: 2000
       }}
       style={styles.shape}
       source={require('./assets/KatoTraje.png')}
@@ -33,17 +36,17 @@ export default function HelloWorld() {
 const styles = StyleSheet.create({
   shape: {
     justifyContent: 'center',
-    height: 250,
-    width: 250,
+    height: 450,
+    width: 350,
     borderRadius: 25,
     marginRight: 10,
-    backgroundColor: 'white',
+    backgroundColor: 'transparent',
   },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
-    backgroundColor: '#9c1aff',
+    backgroundColor: 'blue',
   },
 });
